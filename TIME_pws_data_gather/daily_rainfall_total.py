8a4b9aaf9821785629aa1ba8ccbd0fd7501c7760
Nik Holden <nik.holden99@gmail.com>
2022-02-20 14:01:08 +1300

    update

--- TIME_pws_data_gather/daily_rainfall_total.py
+++ TIME_pws_data_gather/daily_rainfall_total.py
@@ -32,3 +32,12 @@ def daily_rainfall_total():
     cursor.execute(sql_stmt)
     
     conn.commit()
+
+    sql_stmt = """UPDATE weather.total_daily_rainfall SET 
+    current_date_flag = CASE WHEN format(observation_date, 'yyyyMMdd') = format(DATEADD(dd, DATEDIFF(dd, GETUTCDATE(), observation_date),observation_date), 'yyyMMdd') THEN 1 ELSE 0 END
+    ,current_month_flag = CASE WHEN format(observation_date, 'yyyyMM') = format(DATEADD(dd, DATEDIFF(dd, GETUTCDATE(), observation_date),observation_date), 'yyyMM') THEN 1 ELSE 0 END
+    """
+
+    cursor.execute(sql_stmt)
+    
+    conn.commit()
