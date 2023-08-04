84fdcdb0abb5a39486aa704e27c39acbd2a69403
Nik Holden <nik.holden99@gmail.com>
2023-08-04 20:06:39 +1200

    moved client secret to KV

--- TIME_pws_data_gather/config.py
+++ TIME_pws_data_gather/config.py
@@ -3,7 +3,7 @@ from azure.keyvault.secrets import SecretClient
 
 TENANT_ID = "e074f948-369b-4e94-9e9f-043b7464a9db"
 CLIENT_ID = "9729ce94-5a17-474e-89cd-7c0c2fab487d"
-CLIENT_SECRET = "yoz8Q~FPoWiRWAVt0SSH_SLP1E2_EWwEOUNlBadF"
+
 
 KEYVAULT_NAME = 'personal-nh'
 KEYVAULT_URI = f'https://{KEYVAULT_NAME}.vault.azure.net/'
@@ -16,7 +16,7 @@ def db_credentials():
     _credential = ClientSecretCredential(
         tenant_id=TENANT_ID,
         client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET
+        client_secret=_sc.get_secret("nh-azure-db-cs").value
     )
 
     _sc = SecretClient(vault_url=KEYVAULT_URI, credential=_credential)
