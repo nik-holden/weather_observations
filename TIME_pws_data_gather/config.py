bca1434cffa57ed014e4691d3cb38e5457bf8046
Nik Holden <nik.holden99@gmail.com>
2023-08-04 19:25:20 +1200

    updated id

--- TIME_pws_data_gather/config.py
+++ TIME_pws_data_gather/config.py
@@ -3,7 +3,7 @@ from azure.keyvault.secrets import SecretClient
 
 TENANT_ID = "e074f948-369b-4e94-9e9f-043b7464a9db"
 CLIENT_ID = "9729ce94-5a17-474e-89cd-7c0c2fab487d"
-CLIENT_SECRET = "vKl8Q~9i1d0hy_ujFYlb7rlqWCGDwYWyRXvY5c8R"
+CLIENT_SECRET = "yoz8Q~FPoWiRWAVt0SSH_SLP1E2_EWwEOUNlBadF"
 
 KEYVAULT_NAME = 'personal-nh'
 KEYVAULT_URI = f'https://{KEYVAULT_NAME}.vault.azure.net/'
